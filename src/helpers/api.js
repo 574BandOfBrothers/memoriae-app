8e2361ad7b592bc2d37b6c939ca1f20cf626967e
Arda Pektaş <ardaogulcan@gmail.com>
2017-12-20 00:52:19 +0300

    Add token to network requests (#9)

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -71,13 +71,13 @@ export default class Api {
       method: 'post',
       headers: this.header(),
       body: JSON.stringify(data),
-    }).then(handleResponse, handleError);
-
-    // TODO: actual fetch request to the api
-    return Promise.resolve({
-      accessToken: '123',
-      name: 'Arda',
     })
+    .then(handleResponse)
+    .then((response) => {
+      this._token = response.accessToken;
+      return Promise.resolve(response);
+    })
+    .catch(handleError);
   }
 
   signUp(data) {
